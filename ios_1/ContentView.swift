a2d80d131ddc6bce85a0be94c7ef5542bfaf626e
niska223 <fathimaniska12@gmail.com>
2026-06-06 14:39:10 +0530

    Initial Commit

---
+++ ios_1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ios_1
+//
+//  Created by Student1 on 2026-06-06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ios_1/ios_1App.swift
@@ -0,0 +1,17 @@
+//
+//  ios_1App.swift
+//  ios_1
+//
+//  Created by Student1 on 2026-06-06.
+//
+
+import SwiftUI
+
+@main
+struct ios_1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
